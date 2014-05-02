6c836e0a8677c07f066012248607fa0404990abd
kejmil01 <kejmil01@gmail.com>
2014-05-02 12:28:53 +0200

    Background optimalizations

--- StarHunterSTM/StarHunter/SNK/SpaceBackground.cpp
+++ StarHunterSTM/StarHunter/SNK/SpaceBackground.cpp
@@ -1,8 +1,7 @@
 #include "SpaceBackground.h"
 
 SpaceBackground::SpaceBackground(Display *display){
-	for(Sprite *s : tiles)
-		s = 0;
+	tileBitmap = 0;
 	this->display = display;
 	speed =  1.0f;
 	direction = InputManager::MoveEventType::NONE;
@@ -10,73 +9,14 @@ SpaceBackground::SpaceBackground(Display *display){
 }
 
 SpaceBackground::~SpaceBackground(){
-	for(Sprite *s : tiles){
-		delete s;
-	}
+	al_destroy_bitmap(tileBitmap);
 }
 
 void SpaceBackground::initializeTile(){
-	ALLEGRO_BITMAP* tileBitmap = al_create_bitmap(display->getWidth(), display->getHeight());
-	tileCenter = new Sprite(tileBitmap, display->getWidth() / 2.0f, display->getHeight() / 2.0f);
-	al_set_target_bitmap(tileBitmap);
-	al_clear_to_color(al_map_rgb(0, 0, 0));
-	al_set_target_backbuffer(display->getAllegroDisplay());
+	tileBitmap = al_create_bitmap(display->getWidth(), display->getHeight());
 
 	drawStars();
-	setupSprites();
-}
-
-void SpaceBackground::setupSprites(){
-	Sprite *tileUp = new Sprite(*tileCenter);
-	tileUp->setPosition(
-		display->getWidth() / 2.0f,
-		display->getHeight() / 2.0f - display->getHeight());
-
-	Sprite *tileLeft = new Sprite(*tileCenter);
-	tileLeft->setPosition(
-		display->getWidth() / 2.0f - display->getWidth(),
-		display->getHeight() / 2.0f);
-
-	Sprite *tileLeftUp = new Sprite(*tileCenter);
-	tileLeftUp->setPosition(
-		display->getWidth() / 2.0f - display->getWidth(),
-		display->getHeight() / 2.0f - display->getHeight());
-
-	Sprite *tileLeftDown = new Sprite(*tileCenter);
-	tileLeftDown->setPosition(
-		display->getWidth() / 2.0f - display->getWidth(),
-		display->getHeight() / 2.0f + display->getHeight());
-
-	Sprite *tileDown = new Sprite(*tileCenter);
-	tileDown->setPosition(
-		display->getWidth() / 2.0f,
-		display->getHeight() / 2.0f + display->getHeight());
-
-	Sprite *tileRight = new Sprite(*tileCenter);
-	tileRight->setPosition(
-		display->getWidth() / 2.0f + display->getWidth(),
-		display->getHeight() / 2.0f);
-
-	Sprite *tileRightDown = new Sprite(*tileCenter);
-	tileRightDown->setPosition(
-		display->getWidth() / 2.0f + display->getWidth(),
-		display->getHeight() / 2.0f + display->getHeight());
-
-	Sprite *tileRightUp = new Sprite(*tileCenter);
-	tileRightUp->setPosition(
-		display->getWidth() / 2.0f + display->getWidth(),
-		display->getHeight() / 2.0f - display->getHeight());
-
-
-	tiles.push_back(tileUp);
-	tiles.push_back(tileLeft);
-	tiles.push_back(tileLeftUp);
-	tiles.push_back(tileLeftDown);
-	tiles.push_back(tileCenter);
-	tiles.push_back(tileDown);
-	tiles.push_back(tileRight);
-	tiles.push_back(tileRightDown);
-	tiles.push_back(tileRightUp);
+	setupPositions();
 }
 
 void SpaceBackground::drawStars(){
@@ -89,13 +29,14 @@ void SpaceBackground::drawStars(){
 
 	srand(time(NULL));
 	int seedSize = (rand() % 100) + 200;
-	al_set_target_bitmap(tileCenter->getAllegroBitmap());
+	al_set_target_bitmap(tileBitmap);
+	al_clear_to_color(al_map_rgb(0, 0, 0));
 	for(int i = 0; i < seedSize; i++){
 		float scale = randScale();
 		float scaledWidth = starWidth * scale;
 		float scaledHeight = starHeight * scale;
-		int posX = (rand() % (int)(tileCenter->getWidth() - scaledWidth)) + scaledWidth / 2.0f;
-		int posY = (rand() % (int)(tileCenter->getHeight() - scaledHeight)) + scaledHeight / 2.0f;
+		int posX = (rand() % (int)(display->getWidth() - scaledWidth)) + scaledWidth / 2.0f;
+		int posY = (rand() % (int)(display->getHeight() - scaledHeight)) + scaledHeight / 2.0f;
 
 		al_draw_scaled_bitmap(
 			star,
@@ -121,6 +62,54 @@ float SpaceBackground::randScale(){
 		return i / 250.0f;
 }
 
+void SpaceBackground::setupPositions(){
+
+	tilePositions.push_back( // CENTER TILE
+		std::pair<float, float> (
+		display->getWidth() / 2.0f,
+		display->getHeight() / 2.0f));
+
+	tilePositions.push_back( // UP TILE
+		std::pair<float, float> (
+		display->getWidth() / 2.0f,
+		display->getHeight() / 2.0f - display->getHeight()));
+
+	tilePositions.push_back( // LEFT TILE
+		std::pair<float, float> (
+		display->getWidth() / 2.0f - display->getWidth(),
+		display->getHeight() / 2.0f));
+
+	tilePositions.push_back( // LEFT UP TILE
+		std::pair<float, float> (
+		display->getWidth() / 2.0f - display->getWidth(),
+		display->getHeight() / 2.0f - display->getHeight()));
+
+	tilePositions.push_back( // LEFT DOWN TILE
+		std::pair<float, float> (
+		display->getWidth() / 2.0f - display->getWidth(),
+		display->getHeight() / 2.0f + display->getHeight()));
+
+	tilePositions.push_back( // DOWN TILE
+		std::pair<float, float> (
+		display->getWidth() / 2.0f,
+		display->getHeight() / 2.0f + display->getHeight()));
+
+	tilePositions.push_back( // RIGHT TILE
+		std::pair<float, float> (
+		display->getWidth() / 2.0f + display->getWidth(),
+		display->getHeight() / 2.0f));
+
+	tilePositions.push_back( // RIGHT DOWN TILE
+		std::pair<float, float> (
+		display->getWidth() / 2.0f + display->getWidth(),
+		display->getHeight() / 2.0f + display->getHeight()));
+
+	tilePositions.push_back( // RIGHT UP TILE
+		std::pair<float, float> (
+		display->getWidth() / 2.0f + display->getWidth(),
+		display->getHeight() / 2.0f - display->getHeight()));
+}
+
 void SpaceBackground::changeDirection(InputManager::MoveEventType newDirection){
 	direction = newDirection;
 }
@@ -128,16 +117,16 @@ void SpaceBackground::changeDirection(InputManager::MoveEventType newDirection){
 void SpaceBackground::move(){
 	switch(direction){
 	case InputManager::MoveEventType::LEFT:
-		moveTiles(-speed, 0);
-		break;
-	case InputManager::MoveEventType::RIGHT:
 		moveTiles(speed, 0);
 		break;
+	case InputManager::MoveEventType::RIGHT:
+		moveTiles(-speed, 0);
+		break;
 	case InputManager::MoveEventType::UP:
-		moveTiles(0, -speed);
+		moveTiles(0, speed);
 		break;
 	case InputManager::MoveEventType::DOWN:
-		moveTiles(0, speed);
+		moveTiles(0, -speed);
 		break;
 	}
 
@@ -145,25 +134,33 @@ void SpaceBackground::move(){
 }
 
 void SpaceBackground::moveTiles(float distanceX, float distanceY){
-	for(Sprite *s : tiles)
-		s->setPosition(
-		s->getPositionX() - distanceX,
-		s->getPositionY() - distanceY);
+	for(std::pair<float, float> &position : tilePositions){
+		position.first += distanceX;
+		position.second += distanceY;
+	}
 }
 
 void SpaceBackground::keepInBounds(){
-	if(tileCenter->getPositionX() < -tileCenter->getWidth() / 2.0f)
-		moveTiles(-tileCenter->getWidth(), 0);
-	else if(tileCenter->getPositionX() > display->getWidth() + tileCenter->getWidth() / 2.0f)
-		moveTiles(tileCenter->getWidth(), 0);
-	else if(tileCenter->getPositionY() < -display->getHeight() / 2.0f)
-		moveTiles(0, display->getHeight());
-	else if(tileCenter->getPositionY() > display->getHeight() + display->getHeight() / 2.0f)
-		moveTiles(0, display->getHeight());
+	float width, height;
+	width = display->getWidth();
+	height = display->getHeight();
+
+	if(getCenterPosition().first < 0)
+		moveTiles(width, 0);
+	else if(getCenterPosition().first > width / 2.0f)
+		moveTiles(-width, 0);
+	if(getCenterPosition().second < 0)
+		moveTiles(0, height);
+	else if(getCenterPosition().second > height / 2.0f)
+		moveTiles(0, -height);
+}
+
+std::pair<float, float> &SpaceBackground::getCenterPosition(){
+	return tilePositions.front();
 }
 
 void SpaceBackground::draw(){
-	for(Sprite *s : tiles)
-		s->draw();
+	for(std::pair<float, float> position : tilePositions)
+		al_draw_bitmap(tileBitmap, position.first, position.second, 0);
 }
 
--- StarHunterSTM/StarHunter/SNK/SpaceBackground.h
+++ StarHunterSTM/StarHunter/SNK/SpaceBackground.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <time.h>
+#include <iostream>
 #include <vector>
 #include <thread>
 #include "Sprite.h"
@@ -11,14 +12,15 @@
 class SpaceBackground{
 private:
 	Display *display;
-	std::vector<Sprite *> tiles;
-	Sprite *tileCenter;
+	std::vector<std::pair<float, float>> tilePositions;
+	ALLEGRO_BITMAP* tileBitmap;
 	InputManager::MoveEventType direction;
 	float displayWidth, displayHeight;
 	float speed;
 
 	void initializeTile();
-	void setupSprites();
+	void setupPositions();
+	std::pair<float, float> &getCenterPosition();
 	void drawStars();
 	float randScale();
 	void moveTiles(float distanceX, float distanceY);
